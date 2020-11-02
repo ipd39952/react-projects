9cecf9bc720fc58f2d0929f2dd4f6255a2c35fef
Ivaylo Dyakov <ipd3995@gmail.com>
2020-11-02 16:49:40 +0100

    Finished Reviews project

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import Review from "./Review";
+import { FaGithubSquare } from "react-icons/fa";
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>reviews project setup</h2>
+          <FaGithubSquare />
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
