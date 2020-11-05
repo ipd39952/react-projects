1487fa9ef028696e2feb520daefba0f4b0f54110
Ivaylo Dyakov <ipd3995@gmail.com>
2020-11-05 20:07:37 +0100

    Finished Accordion project

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className="info">
+        {data.map((item) => {
+          return <SingleQuestion key={item.id} {...item} />;
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default App;
