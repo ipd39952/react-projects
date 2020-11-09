78297ab5b696136911333802866248e505fab2cc
Ivaylo Dyakov <ipd3995@gmail.com>
2020-11-09 14:47:29 +0100

    Finished Grocery Helper project

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,26 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                <FaEdit onClick={() => editItem(id)} />
+              </button>
+              <button type="button" className="delete-btn">
+                <FaTrash onClick={() => removeItem(id)} />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
